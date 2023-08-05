922208b6581a53f80f6fab3167f3401be84f79f4
Janis Wehen <janiswehen@gmail.com>
2023-08-05 14:07:48 +0200

    feat: experiment run task1

---
+++ experiments/train_cascade2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eux
+
+#SBATCH --job-name=task1-cascade2-unet-train
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=janis.wehen@mattermost
+#SBATCH --partition=gpupro
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=64gb
+#SBATCH --gpus=1
+#SBATCH --time=30:00:00
+#SBATCH --output=logs/cascade2-unet-train_%j.log # %j is job id
+
+user="janis.wehen"
+condaenv="ba"
+script_path="/dhc/home/janis.wehen/ba/models/bachelor-project/experiments"
+log_path="/dhc/home/janis.wehen/ba/models/bachelor-project/experiments/logs/cascade2-unet-train.log"
+
+function source_conda {
+    eval "$(conda shell.bash hook)"
+    source /dhc/home/$user/conda3/bin/activate
+    conda activate $condaenv
+}
+
+function train {
+    cd $script_path
+    python -m unet.train_model --config configs/cascade2_base_config.yaml configs/ds/task01.yaml > $log_path 2>&1
+}
+
+source_conda
+train
--- experiments/train_patch.sh
+++ experiments/train_patch.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -eux
 
-#SBATCH --job-name=task1-full-unet-train
+#SBATCH --job-name=task1-patch-unet-train
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=janis.wehen@mattermost
 #SBATCH --partition=gpupro
 #SBATCH --cpus-per-task=5
 #SBATCH --mem=64gb
 #SBATCH --gpus=1
-#SBATCH --time=20:00:00
-#SBATCH --output=logs/full-unet-train_%j.log # %j is job id
+#SBATCH --time=30:00:00
+#SBATCH --output=logs/patch-unet-train_%j.log # %j is job id
 
 user="janis.wehen"
 condaenv="ba"
 script_path="/dhc/home/janis.wehen/ba/models/bachelor-project/experiments"
-log_path="/dhc/home/janis.wehen/ba/models/bachelor-project/experiments/logs/training_run_1.1full.log"
+log_path="/dhc/home/janis.wehen/ba/models/bachelor-project/experiments/logs/patch-unet-train.log"
 
 function source_conda {
     eval "$(conda shell.bash hook)"
@@ -23,7 +23,7 @@ function source_conda {
 
 function train {
     cd $script_path
-    python -m unet.train_model --config configs/full/full_base_config.yaml configs/full/task01_full_config.yaml > $log_path 2>&1
+    python -m unet.train_model --config configs/patch_base_config.yaml configs/ds/task01.yaml > $log_path 2>&1
 }
 
 source_conda
